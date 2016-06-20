bffff174eebfe045a625da1b1368a97bd743d241
Matumaros <mat@boar.bar>
2016-06-20 10:56:57 +0200

    Made models compatible with SQLAlchemy

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
-class Model(Base):
+class Model:
     __aliases__ = {}
 
     def __init__(self, **kwargs):
@@ -30,7 +30,7 @@ class Model(Base):
         return '<{name}({values})>'.format(name=name, values=values)
 
 
-class Organization(Model):
+class Organization(Base, Model):
     __tablename__ = 'pipedrive_orgs'
 
     id = Column(Integer, primary_key=True)
@@ -47,56 +47,66 @@ class Organization(Model):
     owner_name = Column(String)
 
 
-class OrganizationField(Model):
-    PATH = 'organizationFields'
-    ATTRIBUTES = {
-        'id': None,
-        'key': '',
-        'name': '',
-        'options': [],
-    }
+# class OrganizationField(Base, Model):
+#     __tablename__ = 'organization_fields'
+
+#     PATH = 'organizationFields'
+#     ATTRIBUTES = {
+#         'id': None,
+#         'key': '',
+#         'name': '',
+#         'options': [],
+#     }
 
 
-class Filter(Model):
-    PATH = 'filters'
-    ATTRIBUTES = {
-        'id': None,
-        'name': '',
-        'type': '',
-    }
+# class Filter(Base, Model):
+#     __tablename__ = 'filters'
+
+#     PATH = 'filters'
+#     ATTRIBUTES = {
+#         'id': None,
+#         'name': '',
+#         'type': '',
+#     }
 
 
-class Deal(Model):
-    PATH = 'deals'
-    ATTRIBUTES = {
-        'id': None,
-    }
+# class Deal(Base, Model):
+#     __tablename__ = 'deals'
+
+#     PATH = 'deals'
+#     ATTRIBUTES = {
+#         'id': None,
+#     }
 
 
-class User(Model):
-    PATH = 'users'
-    ATTRIBUTES = {
-        'id': None,
-    }
+# class User(Base, Model):
+#     __tablename__ = 'users'
+
+#     PATH = 'users'
+#     ATTRIBUTES = {
+#         'id': None,
+#     }
 
 
-class Activity(Model):
-    PATH = 'activitiies'
-    ATTRIBUTES = {
-        'id': None,
-        'company_id': '',
-        'user_id': '',
-        'done': '',
-        'type': '',
-        'due_date': '',
-        'add_time': '',
-        'marked_as_done_time': '',
-        'subject': '',
-        'deal_id': '',
-        'org_id': '',
-        'person_id': '',
-        'person_name': '',
-        'org_name': '',
-        'deal_title': '',
-        'owner_name': '',
-    }
+# class Activity(Base, Model):
+#     __tablename__ = 'activities'
+
+#     PATH = 'activities'
+#     ATTRIBUTES = {
+#         'id': None,
+#         'company_id': '',
+#         'user_id': '',
+#         'done': '',
+#         'type': '',
+#         'due_date': '',
+#         'add_time': '',
+#         'marked_as_done_time': '',
+#         'subject': '',
+#         'deal_id': '',
+#         'org_id': '',
+#         'person_id': '',
+#         'person_name': '',
+#         'org_name': '',
+#         'deal_title': '',
+#         'owner_name': '',
+#     }
