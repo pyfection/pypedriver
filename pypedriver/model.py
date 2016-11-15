a1aec8b50ecb2846b419d9f8cc07f4d84c52f6dd
Keegan Morrison <github@keeganm.com>
2016-11-15 11:29:51 -0500

    Added in function to return JSON formatted data. Useful when accessing custom attributes that have an identifier which starts with a number

--- pypedriver/model.py
+++ pypedriver/model.py
@@ -82,6 +82,12 @@ class Model:
         values = '; '.join(attributes)
         return '<{name}({values})>'.format(name=self.__name, values=values)
 
+    def __json__(self):
+        ret = {}
+        for key, value in self.__attributes.items():
+            ret[str(key)] = str(value);
+        return ret
+
     def get_field_key(self, name):
         """Translate field key
 
